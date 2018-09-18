e777888aa96a2b6381a5f04d396cd58826e7ba1b
Cephisodotus <cephisodotus@ufl.edu>
2018-09-17 22:36:59 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+//This file holds any configuration variables we may need 
+//'config.js' is ignored by git to protect sensitive information, such as your database's username and password
+module.exports = {
+  db: {
+    uri: 'mongodb://CEN3031:CEN3031TA@ds016068.mlab.com:16068/coolcodingcrew', //place the URI of your mongo database here.
+  }
+};
+
+/* Now go to the JSONtoMongo.js file and include this file as a variable named 'config' with a require() 
+
+mongodb://cortiesdie:ChetUbetcha14@ds016068.mlab.com:16068/coolcodingcrew
+*/
